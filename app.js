dccb48f14aec219ddccb981d45ede8f3f40fdb4c
ObodoakorDavid <obodoakordavid@outlook.com>
2023-03-31 10:55:27 +0100

    nav done and dusted

--- app.js
+++ app.js
@@ -3,22 +3,15 @@
 const hamburger = document.querySelector(".hamburger");
 const bars = document.querySelectorAll(".hamburger span");
 const ul = document.querySelector("nav ul");
+const main = document.querySelector("main");
+const logo = document.querySelector(".logo");
 
 hamburger.addEventListener("click", () => {
   bars.forEach((bar) => {
     bar.classList.toggle("active");
   });
   ul.classList.toggle("active");
+  main.classList.toggle("active");
+  logo.classList.toggle("active");
   console.log(ul.classList);
-});
-
-document.body.addEventListener("click", () => {
-//   console.log(ul.classList.contains("active"));
-  if (!ul.classList.contains("active")) {
-    bars.forEach((bar) => {
-      bar.classList.remove("active");
-    });
-    ul.classList.remove("active");
-    console.log("zz");
-  }
 });
